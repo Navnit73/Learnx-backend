7cc98062158c500eccf0ce0ff15866dd958f52fc
Abhishek Singh <meabhisingh25@gmail.com>
2022-07-23 14:03:53 +0530

    LogoutPost

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -28,7 +28,7 @@ router.route("/register").post(singleUpload, register);
 router.route("/login").post(login);
 
 // logout
-router.route("/logout").get(logout);
+router.route("/logout").post(logout);
 
 // Get my profile
 router.route("/me").get(isAuthenticated, getMyProfile);
