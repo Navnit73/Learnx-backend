7d419664bdd28f411601be43721313a863c42130
Abhishek Singh <meabhisingh25@gmail.com>
2022-07-24 14:19:10 +0530

    happy

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -3,7 +3,8 @@ export const sendToken = (res, user, message, statusCode = 200) => {
   const options = {
     expires: new Date(Date.now() + 15 * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    domain: process.env.FRONTEND_DOMAIN,
+    secure: true,
+    sameSite: "none",
   };
 
   res.status(statusCode).cookie("token", token, options).json({
